b0bdc849f782e192f80fd56f44ad315fc6a07495
sean <sean.g.crim@gmail.com>
2019-09-27 16:31:49 -0400

    initial commit for spring tutorial, set up environment and then went through basic steps to display spring process and inversion of control

---
+++ spring-demo-one/src/com/luv2code/springdemo/BaseballCoach.java
@@ -0,0 +1,10 @@
+package com.luv2code.springdemo;
+
+public class BaseballCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "30 minutes in the batting cage :)";
+	}
+	
+}
---
+++ spring-demo-one/src/com/luv2code/springdemo/FencingCoach.java
@@ -0,0 +1,10 @@
+package com.luv2code.springdemo;
+
+public class FencingCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "2 days of fencing now!!!";
+	}
+
+}
